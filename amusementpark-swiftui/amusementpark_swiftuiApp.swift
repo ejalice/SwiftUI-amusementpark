c0929f6f0816679d504f61035a015e254a1f294d
ejalice <ejalice123@naver.com>
2022-05-02 15:32:05 +0900

    Create Project

---
+++ amusementpark-swiftui/amusementpark_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  amusementpark_swiftuiApp.swift
+//  amusementpark-swiftui
+//
+//  Created by eunji on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct amusementpark_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
