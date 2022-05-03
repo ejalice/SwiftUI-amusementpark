a30e476fa770ca895ae0a046a054b6a885dadb62
ejalice <ejalice123@naver.com>
2022-05-03 23:43:23 +0900

    Create DataSets

--- amusementpark-swiftui/Views/SquareView.swift
+++ amusementpark-swiftui/Views/SquareView.swift
@@ -10,36 +10,18 @@ import SwiftUI
 struct SquareView: View {
     @State var changeView : Bool = false
     @State var isLiked: Bool = false
+    
+    @Binding var picture_code: String
+    @Binding var name: String
+    
+    
     var body: some View {
-//        //ZStack {
-////            RoundedRectangle(cornerRadius: 10)
-////                .fill(Color.white)
-////                .frame(width: 150, height: 170)
-////                .shadow(color: .myGray, radius: 2, x: 0, y: 1)
-//            VStack {
-//                Image("드라켄")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(width: 150, height: 100)
-//                HStack {
-//                    Text("드라켄")
-//                    Image(systemName: "heart.fill")
-//                } //~HStack
-//            }
-//            .padding(.bottom, 9) //~VStack
-//            .border(Color.myGray, width: 1)
-//            .shadow(color: .myGray, radius: 2, x: 0, y: 2)
-//        //} //~ZStack
-//        Button(action: {
-//            self.isTapped.toggle()
-//        }) {
-        //NavigationLink(destination: AttractionDetailView()) {
-            //ZStack {
+
                 VStack {
                     Button(action: {
                         self.changeView.toggle()
                     }) {
-                        Image("드라켄")
+                        Image("\(picture_code)")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 150, height: 100)
@@ -47,7 +29,7 @@ struct SquareView: View {
                     NavigationLink(destination: AttractionDetailView(), isActive: $changeView) {EmptyView()}.disabled(true)
                     
                     HStack {
-                        Text("Title")
+                        Text("\(name)")
                         Button(action: {
                             self.isLiked.toggle()
                         }){
@@ -56,33 +38,12 @@ struct SquareView: View {
                         }
                     }
                 }
-            //}
-        //}
-//            VStack {
-//                Image("드라켄")
-//                    .resizable()
-//                    .aspectRatio(contentMode: .fit)
-//                    .frame(width: 150, height: 100)
-//
-//                HStack {
-//                    Text("드라켄")
-//                    Button(action: {
-//                        self.isLiked.toggle()
-//                    }){
-//                        Image(systemName: self.isLiked == true ? "heart.fill" : "heart")
-//                            .foregroundColor(Color.myRed)
-//                    }
-//                }
-//            }
-//            .padding(.bottom, 9)
-//            .border(Color.myGray, width: 1)
-            //.shadow(color: .myGray, radius: 2, x: 0, y: 2)
-        //}
+        
     }
 }
 
-struct SquareView_Previews: PreviewProvider {
-    static var previews: some View {
-        SquareView()
-    }
-}
+//struct SquareView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SquareView()
+//    }
+//}
